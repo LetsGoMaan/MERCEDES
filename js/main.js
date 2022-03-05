216d67cba45f6ce32538127ffedd34ace6feb826
LetsGoMan <ilikeseik@gmail.com>
2022-03-05 17:35:44 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ $('.design-slider').slick({
   variableWidth: true,
   prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="" />',
   nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="" />',
+  responsive : [
+    {
+      breakpoint: 361,
+      settings: {
+        variableWidth: false,
+        slidesToShow: 1,
+
+      }
+    }
+  ]
 });
 });
